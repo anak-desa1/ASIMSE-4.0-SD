dd4e87ae182c160bce494b193ec2313a2509830e
anak-desa1 <anakdesa143@gmail.com>
2023-08-01 09:39:33 +0700

    update-01-08-2023
    update-01-08-2023

--- application/modules/perpus_data/views/data_label/v_scan_kosong.php
+++
@@ -1,9 +0,0 @@
-          <div class="box box-widget">
-              <div class="box-header with-border">
-                  <div class="user-block">
-                      <span class="username"><a href="#">DATA TIDAK DITEMUKAN!</a></span>
-                  </div>
-              </div>
-              <div class="box-body">
-              </div>
-          </div>
